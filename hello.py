f49f97692301bbfd36a1e25300ed8aeb03bf1da7
ImKunYoung <lim8667@gmail.com>
2022-08-02 19:41:43 +0900

    init commit

--- hello.py
+++ hello.py
@@ -1,1 +1,28 @@
 print("Hello. World!")
+print("Hello. World!")
+
+# 문자열
+
+# str 문자열 클래스
+# type("Hello. World!")
+# <class 'str'>
+
+# type('Hello. World!')
+# <class 'str'>
+
+
+# 이스케이프 문자
+# print('C:\Windows\System32\notepad.exe')
+# C:\Windows\System32
+# otepad.exe
+
+# print(r'C:\Windows\System32\notepad.exe')
+# C:\Windows\System32\notepad.exe
+
+# print('\"It\'s not that I\'m so smart; it\'s just that I stay with problems longer.\" Albert Einstein')
+# "It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein
+
+# print('''"It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein''')
+# "It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein
+
+# TODO - p21
