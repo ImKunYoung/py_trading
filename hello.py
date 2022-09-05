22b7ebc0d21a6da88e82432af81c7e899660ec27
ImKunYoung <lim8667@gmail.com>
2022-09-05 21:27:49 +0900

    주석

--- hello.py
+++ hello.py
@@ -16,13 +16,24 @@ print("Hello. World!")
 # C:\Windows\System32
 # otepad.exe
 
-# print(r'C:\Windows\System32\notepad.exe')
+print(r'C:\Windows\System32\notepad.exe')
 # C:\Windows\System32\notepad.exe
 
-# print('\"It\'s not that I\'m so smart; it\'s just that I stay with problems longer.\" Albert Einstein')
+print('\"It\'s not that I\'m so smart; it\'s just that I stay with problems longer.\" Albert Einstein')
 # "It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein
 
-# print('''"It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein''')
+print('''"It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein''')
 # "It's not that I'm so smart; it's just that I stay with problems longer." Albert Einstein
 
-# TODO - p21
+# 여러 줄에 걸친 문자열을 나타낼 때
+print('''Wake up, Neo... 
+The Matrix has you...
+Follow the white rabbit.''')
+
+# 주석
+'''
+Wake up, Neo... 
+The Matrix has you...
+Follow the white rabbit.
+'''
+
