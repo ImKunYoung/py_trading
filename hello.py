8d011f71509bcaa795433c46af3b3f911eaad385
ImKunYoung <lim8667@gmail.com>
2022-09-05 21:27:57 +0900

    인덱싱

--- hello.py
+++ hello.py
@@ -37,3 +37,13 @@ The Matrix has you...
 Follow the white rabbit.
 '''
 
+# 인덱싱
+print('***************인덱싱*****************')
+word = 'Python'
+print(len(word))
+print(word[0])
+print(word[0]+word[1]+word[2]+word[3]+word[4]+word[5])
+
+print(word[-6])
+print(word[-6]+word[-5]+word[-4]+word[-3]+word[-2]+word[-1])
+
